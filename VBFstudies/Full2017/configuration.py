4e8863f36e476d469fbb14cead50f1072d342048
Luis Junior Sánchez Rosas <lusanche@users.noreply.github.com>
2019-06-05 16:31:29 -0300

    Add files via upload

---
+++ VBFstudies/Full2017/configuration.py
@@ -0,0 +1,33 @@
+# example of configuration file
+treeName = 'Events'
+ 
+# ===== sub categories
+tag = 'lowmjj_detajj'
+#tag = 'lowmjj_nodetajj'
+#tag = 'highmjj_detajj'
+#tag = 'highmjj_nodetajj'
+
+# used by mkShape to define output directory for root files
+outputDir = 'rootFile_'+tag
+
+# file with TTree aliases and list of variables/cuts/samples/plots
+aliasesFile = 'aliases.py'
+variablesFile = 'variables.py'
+cutsFile = 'cuts.py'
+samplesFile = 'samples.py' 
+plotFile = 'plot.py' 
+
+# luminosity to normalize to (in 1/fb)
+lumi = 41.5
+
+# used by mkPlot/mkDatacards to define output directory for plots/datacards
+# different from "outputDir" to do things more tidy
+outputDirPlots = 'plot_'+tag
+outputDirDatacard = 'datacards_'+tag
+
+# structure file for datacard
+structureFile = 'structure.py'
+
+# nuisances file for mkDatacards/mkShape
+nuisancesFile = 'nuisances.py'
+
---
+++ VBFstudies/Full2017/plot.py
@@ -0,0 +1,259 @@
+# plot configuration
+
+
+
+# groupPlot = {}
+# 
+# Groups of samples to improve the plots.
+# If not defined, normal plots is used
+#
+
+groupPlot['top']  = {
+                  'nameHR' : 'tW and t#bar{t}',
+                  'isSignal' : 0,
+                  'color': 400,   # kYellow
+                  'samples'  : ['top']
+              }
+
+groupPlot['WW']  = {
+                  'nameHR' : 'WW',
+                  'isSignal' : 0,
+                  'color': 851, # kAzure -9 
+                  'samples'  : ['WW', 'ggWW']
+              }
+
+groupPlot['Fake']  = {  
+                  'nameHR' : 'Non-prompt',
+                  'isSignal' : 0,
+                  'color': 921,    # kGray + 1
+                  'samples'  : ['Fake']
+              }
+
+
+groupPlot['DY']  = {  
+                  'nameHR' : "DY",
+                  'isSignal' : 0,
+                  'color': 418,    # kGreen+2
+                  'samples'  : ['DY']
+              }
+
+groupPlot['VVV']  = {  
+                  'nameHR' : 'VVV',
+                  'isSignal' : 0,
+                  'color': 857, # kAzure -3  
+                  'samples'  : ['VVV']
+              }
+
+groupPlot['VZ']  = {  
+                  'nameHR' : "VZ",
+                  'isSignal' : 0,
+                  'color'    : 617,   # kViolet + 1  
+                  'samples'  : ['VZ', 'WZ', 'ZZ']
+              }
+
+groupPlot['Vg']  = {  
+                  'nameHR' : "V#gamma",
+                  'isSignal' : 0,
+                  'color'    : 810,   # kOrange + 10
+                  'samples'  : ['Vg', 'Wg']
+              }
+
+groupPlot['WZgS']  = { #VgS
+                  'nameHR' : "V#gamma*",
+                  'isSignal' : 0,
+                  'color'    : 409,   # kGreen - 9
+                  'samples'  : ['WZgS']
+              }
+
+#==============================================================
+
+groupPlot['Higgs']  = {
+                  'nameHR' : 'Higgs',
+                  'isSignal' : 1,
+                  'color': 632, # kRed 
+                  'samples' : ['qqH_hww','ggH_hww','H_htt', 'ZH_hww', 'ggZH_hww', 'WH_hww','ttH_hww','ZH_htt', 'WH_htt', 'qqH_htt', 'ggH_htt']
+              }
+
+#groupPlot['Higgs1']  = {
+#                  'nameHR' : 'VBF',
+#                  'isSignal' : 1,
+#                  'color': 617, # kViolet
+#                  'samples' : ['qqH_hww']
+#              }
+#
+#groupPlot['Higgs2']  = {
+#                  'nameHR' : 'GGF',
+#                  'isSignal' : 1,
+#                  'color': 857, # kRed 
+#                  'samples'  : ['ggH_hww']
+#              }
+#
+#groupPlot['Higgs3']  = {
+#                  'nameHR' : 'others HPM',
+#                  'isSignal' : 1,
+#                  'color': 632, # kRed 
+#                  'samples' : ['H_htt', 'ZH_hww', 'ggZH_hww', 'WH_hww','ttH_hww','ZH_htt', 'WH_htt', 'qqH_htt', 'ggH_htt']
+#              }
+#
+
+#plot = {}
+
+# keys here must match keys in samples.py    
+#                    
+plot['DY']  = {  
+                  'color': 418,    # kGreen+2
+                  'isSignal' : 0,
+                  'isData'   : 0, 
+                  'scale'    : 1.0*1.7986,
+              }
+
+plot['Fake']  = {  
+                  'color': 921,    # kGray + 1
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  'scale'    : 1.0                  
+              }
+
+plot['top'] = {   
+                  'nameHR' : 'tW and t#bar{t}',
+                  'color': 400,   # kYellow
+                  'isSignal' : 0,
+                  'isData'   : 0, 
+                  'scale'    : 1.0*1.1572,
+                  }
+
+plot['WW']  = {
+                  'color': 851, # kAzure -9 
+                  'isSignal' : 0,
+                  'isData'   : 0,    
+                  'scale'    : 1.0   # ele/mu trigger efficiency   datadriven
+                  }
+
+plot['ggWW']  = {
+                  'color': 850, # kAzure -10
+                  'isSignal' : 0,
+                  'isData'   : 0,    
+                  'scale'    : 1.0
+                  }
+
+plot['Vg']  = { 
+                  'color': 859, # kAzure -1  
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  'scale'    : 1.0
+                  }
+
+plot['VZ']  = { 
+                  'color': 858, # kAzure -2  
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  'scale'    : 1.0
+                  }
+
+plot['VVV']  = { 
+                  'color': 857, # kAzure -3  
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  'scale'    : 1.0
+                  }
+
+plot['WZgS']  = {
+                  'color': 617, # kViolet + 1
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  'scale'    : 1.0
+                  }
+
+# Htautau
+
+plot['H_htt'] = {
+                  'nameHR' : 'Htt',
+                  'color': 632+4, # kRed+4 
+                  'isSignal' : 1,
+#                  'isSignal' : 0,
+                  'isData'   : 0, 
+                  'scale'    : 1    #
+                  }
+
+
+# HWW 
+
+plot['ZH_hww'] = {
+                  'nameHR' : 'ZH',
+                  'color': 632+3, # kRed+3 
+                  'isSignal' : 1,
+#                  'isSignal' : 0,
+                  'isData'   : 0,    
+                  'scale'    : 1    #
+                  }
+
+plot['ggZH_hww'] = {
+                  'nameHR' : 'ggZH',
+                  'color': 632+4, # kRed+4
+                  'isSignal' : 1,
+#                  'isSignal' : 0,
+                  'isData'   : 0,    
+                  'scale'    : 1    #
+                  }
+
+plot['WH_hww'] = {
+                  'nameHR' : 'WH',
+                  'color': 632+2, # kRed+2 
+                  'isSignal' : 1,
+#                  'isSignal' : 0,
+                  'isData'   : 0,    
+                  'scale'    : 1    #
+                  }
+
+
+plot['qqH_hww'] = {
+                  'nameHR' : 'qqH',
+                  'color': 632+1, # kRed+1 
+                  'isSignal' : 1,
+                  'isData'   : 0,    
+                  'scale'    : 1    #
+                  }
+
+
+plot['ggH_hww'] = {
+                  'nameHR' : 'ggH',
+                  'color': 632, # kRed 
+                  'isSignal' : 1,
+#                  'isSignal' : 0,
+                  'isData'   : 0,    
+                  'scale'    : 1    #
+                  }
+
+
+plot['ttH_hww'] = {
+                  'nameHR' : 'ttH',
+                  'color': 632+6, # kRed+6
+                  'isSignal' : 1,
+#                  'isSignal' : 0,
+                  'isData'   : 0,
+                  'scale'    : 1    #
+                  }
+
+
+# data
+
+plot['DATA']  = { 
+                  'nameHR' : 'Data',
+                  'color': 1 ,  
+                  'isSignal' : 0,
+                  'isData'   : 1 ,
+                  'isBlind'  : 1
+              }
+
+
+
+
+# additional options
+
+#legend['lumi'] = 'L = 35.9/fb'
+legend['lumi'] = 'L = 41.5/fb'
+
+legend['sqrt'] = '#sqrt{s} = 13 TeV'
+
+
+
